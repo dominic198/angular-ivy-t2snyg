2e81361e308702f984b397f903e916fa6dceffb8
dominic198 <dominicsavio8@gmail.com>
2022-06-06 11:05:53 +0530

    2 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit, VERSION } from '@angular/core';
+import { AfterViewInit, Component, OnInit, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent  {
+export class HomeComponent  implements AfterViewInit{
+
+  ngAfterViewInit(): void {
+    this.sh = "0"
+    this.isChecked = true;
+  }
 
   name = 'Angular ' + VERSION.major;
   sh = ""
