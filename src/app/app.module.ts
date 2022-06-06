3a62cc6db1d43b7a95d36c84d92a0fb59ea893bb
dominic198 <dominicsavio8@gmail.com>
2022-06-06 10:35:58 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ValidationComponent } from './validation/validation.component';
 import { AppRouter } from '../app-router';
 import { HomeComponent } from './home/home.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRouter, ReactiveFormsModule ],
@@ -15,7 +16,8 @@ import { CalendarComponent } from './calendar/calendar.component';
     HelloComponent, 
     ValidationComponent,
     HomeComponent,
-    CalendarComponent
+    CalendarComponent,
+    HeaderComponent
   ],
   bootstrap:    [ AppComponent ]
 })
